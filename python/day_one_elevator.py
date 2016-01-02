85f4f3327916134d6d3cd4072bd776ab8822b905
Chad A. Williams <chadgit13@gmail.com>
2016-01-02 16:15:58 -0500

    Python part 2 of day 1

--- python/day_one_elevator.py
+++ python/day_one_elevator.py
@@ -1,8 +1,16 @@
-directions = input("Enter elevator directions:\n")
+filename = "..\inputs\day_one_input.txt"
+f = open(filename)
+directions = f.readline()
 cur_floor = 0
+direction_num = 0
+basement_reached = False
 for c in directions:
+	direction_num += 1
 	if c == ")":
 		cur_floor -= 1
 	elif c == "(":
 		cur_floor += 1
+	if cur_floor < 0 and not basement_reached:
+		print("Reached basement with direction num: "+str(direction_num))
+		basement_reached = True
 print("Resulting floor "+str(cur_floor));
