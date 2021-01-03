b415a7d4a31a151af421ace55ec33b96fccf5381
Chad Williams <chadgit13@gmail.com>
2021-01-03 08:38:27 -0500

    Added performance improvement to day one part 2

--- python/2020/day_one_part2.py
+++ python/2020/day_one_part2.py
@@ -7,8 +7,10 @@ for line in lines:
     numbers.append(int(line))
 for i in range(len(numbers)):
     for j in range(i+1,len(numbers)):
-        for k in range(j+1,len(numbers)):
-            if (numbers[i] + numbers[j] + numbers[k]) == 2020:
-                print(numbers[i] * numbers[j] * numbers[k])
-                exit()
-
+        # Assuming here all numbers are >= 0, if not true this optimization should be removed
+        if (numbers[i] + numbers[j]) < 2020:
+            for k in range(j+1,len(numbers)):
+                if (numbers[i] + numbers[j] + numbers[k]) == 2020:
+                    print(numbers[i] * numbers[j] * numbers[k])
+                    exit()
+print("No solution found")
