3d03c51d4d9c32bcbc1bb8e5d5174967030acc73
Chad Williams <chadgit13@gmail.com>
2021-01-04 12:59:29 -0500

    Day 5 complete part 1 and 2 same file

--- python/2020/day_five_part1-2.py
+++ python/2020/day_five_part1-2.py
@@ -15,12 +15,21 @@ filename = "inputs\\2020\\input-day5.txt"
 with open(filename) as f:
     lines = f.readlines()
 highestSeatId = -1
+seatIds = []
 
 for line in lines:
 	row = binaryPartition(line[0:7],0,127)
 	col = binaryPartition(line[7:],0,7)
 	seatId = (row * 8) + col
+	seatIds.append(seatId)
 	#print("Result:  row:"+str(row)+"  col:"+str(col) + " seatId:"+str(seatId))
 	if (seatId > highestSeatId):
     		highestSeatId = seatId
 print("Highest Seat ID: "+str(highestSeatId))
+seatIds.sort()
+prevId = seatIds[0]
+for curId in seatIds[1:]:
+	if curId > (prevId + 1):
+		print("Your seat id is: "+str(curId-1))
+		break
+	prevId = curId
