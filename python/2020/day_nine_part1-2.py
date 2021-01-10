1b51023de1d99308e205c687f27b31f9f68c0d73
Chad Williams <chadgit13@gmail.com>
2021-01-10 09:07:24 -0500

    Day 9 parts 1 and 2 complete

--- python/2020/day_nine_part1-2.py
+++ python/2020/day_nine_part1-2.py
@@ -19,9 +19,35 @@ with open(filename) as f:
 listNumbers = list(map(int, lines)) 
 
 windowSize = 25
+invalidNumber = 0
 
 # Starting at curIndex check each number through the end of the list
 for curIndex in range(windowSize,len(listNumbers)):
     matchFound = checkCurNum(listNumbers[curIndex-windowSize:curIndex+1],listNumbers[curIndex])
     if not matchFound:
         print("Number " + str(listNumbers[curIndex]) + " is invalid")
+        invalidNumber = listNumbers[curIndex]
+
+startIndex = 0
+endIndex = 0
+
+# Find contiguous range that adds to invalid number
+for curIndex in range(len(listNumbers)):
+    sum = listNumbers[curIndex]
+    sumFound = False
+    for secondIndex in range(curIndex+1, len(listNumbers)):
+        sum += listNumbers[secondIndex]
+        if sum == invalidNumber:
+            startIndex = curIndex
+            endIndex = secondIndex
+            sumFound = True
+            break
+        elif sum > invalidNumber:
+            break
+    if sumFound:
+        break
+
+print("Match list: " + str(listNumbers[startIndex:endIndex+1]))
+minNum = min(listNumbers[startIndex:endIndex+1])
+maxNum = max(listNumbers[startIndex:endIndex+1])
+print("Weakness: " + str(minNum + maxNum))
